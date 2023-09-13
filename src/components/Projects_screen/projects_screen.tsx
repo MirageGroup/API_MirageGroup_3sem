a09c63ff9f32c58940f946e97e3dc6f8e5882631
Gustavo Henrique <108089562+gustavohpereira@users.noreply.github.com>
2023-09-13 09:22:35 -0300

    feature/card_list

--- src/components/Projects_screen/projects_screen.tsx
+++ src/components/Projects_screen/projects_screen.tsx
@@ -3,17 +3,46 @@ import './projects_screen_style.scss'
 
 export function ProjectScreen(){
 
-    const lista_projetos = ["projeto1","projeto2","projeto3","projeto4"]
+    const lista_projetos = [
+    { id: 1, name: "projeto1", finished: true },
+    { id: 2, name: "projeto2", finished: true },
+    { id: 3, name: "projeto3", finished: false },
+    { id: 4, name: "projeto4", finished: true },
+    { id: 5, name: "projeto5", finished: true },
+    { id: 1, name: "projeto1", finished: true },
+    { id: 2, name: "projeto2", finished: true },
+    { id: 3, name: "projeto3", finished: false },
+    { id: 4, name: "projeto4", finished: true },
+    { id: 5, name: "projeto5", finished: false },
+    ]
 
     return(
         <div className="screen_wrapper">
-            <h3>não finalizados</h3>
-            <div className="project_list">
-                {lista_projetos.map(
-                    (e) => <ProjectCard name={e}></ProjectCard>
-                    )
-                }
+            <div className="list_container">
+                <h3>Em andamento</h3>
+                <span></span>
+                <div className="project_list">
+                    {lista_projetos.map(
+                        (projeto) => projeto.finished? <ProjectCard name={projeto.name}></ProjectCard>:null  
+                        )
+                    }
+                </div>
+
             </div>
+
+            <div className="list_container">
+                <h3>Concluidos</h3>
+                <span></span>
+                <div className="project_list">
+                    {lista_projetos.map(
+                        (projeto) => projeto.finished?null: <ProjectCard name={projeto.name}></ProjectCard>  
+                        )
+                    }
+                </div>
+
+            </div>
+            
+            
             
         </div>
     )
