baa529ed9923fbb05bfadd2932bd3e8b59b14d87
Gustavo <gustavodossantos1704@gmail.com>
2023-09-24 17:55:27 -0300

    :sparkles: feat: task creation form

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,21 @@ import { ProjectScreen } from './components/Projects_screen/projects_screen'
 import { Sidebar } from './components/sidebar/sidebar';
 import { Home_page } from './components/home_page/home_page'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { New_task } from './components/New_task/New_task'
 
 function App() {
 
   return (
-    <Router>
-      <Sidebar></Sidebar>
-      <Routes>
-        <Route path='/screen' Component={Screen}/>
-        <Route path='/project_screen' Component={ProjectScreen}/>
-        <Route path='/' Component={Home_page}/>
-        <Route path='/screen' Component={Screen}/>
-      </Routes>
-    </Router> 
-    
+    // <Router>
+    //   <Sidebar></Sidebar>
+    //   <Routes>
+    //     <Route path='/screen' Component={Screen}/>
+    //     <Route path='/project_screen' Component={ProjectScreen}/>
+    //     <Route path='/' Component={Home_page}/>
+    //     <Route path='/screen' Component={Screen}/>
+    //   </Routes>
+    // </Router> 
+    <New_task></New_task>
   )
 }
 
