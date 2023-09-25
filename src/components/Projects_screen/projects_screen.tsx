71afa15a3cf0adaa53ecd9f52cd957b0d2680347
gustavohpereira <gustavohpa2003@gmail.com>
2023-09-24 21:02:58 -0300

    feature/create project list

--- src/components/Projects_screen/projects_screen.tsx
+++ src/components/Projects_screen/projects_screen.tsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ProjectCard } from "../Project_card/project_card";
 import './projects_screen_style.scss'
 import { Process_card } from "../Process_Card/process_card";
 import * as FaIcons from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import axios from "axios";
 
 export function ProjectScreen(){
 
-    const lista_projetos = [
+
+
+
+    let lista_projetos = [
     { id: 1, name: "projeto1", finished: true },
     { id: 2, name: "projeto2", finished: true },
     { id: 3, name: "projeto3", finished: false },
@@ -30,6 +34,43 @@ export function ProjectScreen(){
     { id: 5, name: "projeto5", finished: false },
     
     ]
+    const fetchProcesses = async () => {
+        try {
+          const response = await axios.get('http://localhost:8000/process/findall');
+          return response.data;
+        } catch (error) {
+          console.error('Error fetching processes:', error);
+          throw error;
+        }
+      };
+
+    const [processes, setProcesses] = useState<any[]>([]);
+
+    useEffect(() => {
+        // Function to fetch and update processes
+        const updateProcesses = async () => {
+          try {
+            const updatedProcesses = await fetchProcesses();
+            setProcesses(updatedProcesses);
+          } catch (error) {
+            // Handle any errors
+          }
+        };
+      
+        // Poll for updates every 5 seconds (adjust the interval as needed)
+        const pollInterval = setInterval(updateProcesses, 300);
+      
+        // Clean up the interval when the component unmounts
+        return () => clearInterval(pollInterval);
+      }, []); // The empty dependency array ensures this effect runs only once on component mount
+    
+
+      const finished_process = [
+        {name: "projeto opera"},
+        {name: "projeto blues"},
+        {name: "projeto sky"},
+        {name: "projeto flag"},
+      ]
 
     const [isModalOpen, setIsModalOpen] = useState(false); 
 
@@ -57,8 +98,8 @@ export function ProjectScreen(){
                 <h3>Em andamento</h3>
                 <span></span>
                 <div className="project_list">
-                    {lista_projetos.map(
-                        (projeto) => projeto.finished? <ProjectCard name={projeto.name}></ProjectCard>:null  
+                    {processes.map(
+                        (projeto) =><ProjectCard name={projeto.name}></ProjectCard>
                         )
                     }
             </div>
@@ -69,8 +110,8 @@ export function ProjectScreen(){
                 <h3>Concluidos</h3>
                 <span></span>
                 <div className="project_list">
-                    {lista_projetos.map(
-                        (projeto) => projeto.finished?null: <ProjectCard name={projeto.name}></ProjectCard>  
+                    {finished_process.map(
+                        (projeto) => <ProjectCard name={projeto.name}></ProjectCard>  
                         )
                     }
                 </div>
