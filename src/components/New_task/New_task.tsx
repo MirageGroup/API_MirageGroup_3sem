9d2e8f95ead363177195a630db18bb3011ed7734
Gustavo <gustavodossantos1704@gmail.com>
2023-09-26 16:00:35 -0300

    style: changing the font
    i've changed the font-family on the 'new_task' screen'

--- src/components/New_task/New_task.tsx
+++ src/components/New_task/New_task.tsx
@@ -1,5 +1,7 @@
 import './New_task.scss';
 import {AiOutlineClose} from 'react-icons/ai'
+import {IoMdClose} from 'react-icons/io'
+
 
 export function New_task() {
     return (
@@ -7,26 +9,33 @@ export function New_task() {
             
             <div className='form-container'>
                 <div className='form-title'><h3>Nova Tarefa</h3></div>
-                <div className='close-button'><AiOutlineClose size={30}/></div>
+                <button className='close-button'><IoMdClose size={35} className='button'/></button>
                 <form>
                     <input type="text" name="task-name" className='task-name'  placeholder='Nome da tarefa'/>
+
                     <input type="text" name="responsible-name" className='responsible-name' placeholder='Responsável principal'/>
-                    <label>Nível de Prioridade</label>
-                    <div className='priority-buttons'>
-                        <input type="radio" id="verde" name="prioridade" value="verde"/>
-                        <label className="green" ></label>
-            
-                        <input type="radio" id="amarelo" name="prioridade" value="amarelo"/>
-                        <label className="yellow" ></label>
-                        
-                        <input type="radio" id="vermelho" name="prioridade" value="vermelho"/>
-                        <label className="red" ></label>
+
+                    <div className="priority">
+                        <label>Nível de Prioridade</label>
+                        <div className='priority-buttons'>
+                            <input type="radio" id="verde" name="prioridade" value="verde"/>
+                            <label className="green" ></label>
+                
+                            <input type="radio" id="amarelo" name="prioridade" value="amarelo"/>
+                            <label className="yellow" ></label>
+                            
+                            <input type="radio" id="vermelho" name="prioridade" value="vermelho"/>
+                            <label className="red" ></label>
+                        </div>
                     </div>
+
                     <div className='date-container'>
                         <label>Data Limite:</label>
                         <input type="date" />
                     </div>
-                    <textarea placeholder='Descrição' rows={23} cols={30}></textarea>
+
+                    <textarea placeholder='Descrição' rows={15} cols={30}></textarea>
+                    
                 </form>
             </div>
         </>
