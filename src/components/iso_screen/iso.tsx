0156168d4724f90386096457c4d3251b72faa2ac
gustavohpereira <gustavohpa2003@gmail.com>
2023-10-08 22:01:13 -0300

    modal_creation

--- src/components/iso_screen/iso.tsx
+++ src/components/iso_screen/iso.tsx
@@ -1,12 +1,28 @@
 import './iso_style.scss'
 import PlusImage from '../../assets/plus-solid.png';
+import { Process_card } from '../Process_Card/process_card';
+import { FiX } from 'react-icons/fi';
+import { useState } from 'react';
+import { IsoForm } from '../../isoFormulary/isoForm';
 
 export function IsoScreen(){
+
+    const [isModalOpen, setIsModalOpen] = useState(false); 
+
+    const openModal = () => {
+        setIsModalOpen(true);
+      };
+    
+      const closeModal = () => {
+        setIsModalOpen(false);
+      };
+
+
     return(
 
         <div className="iso_screen_wrapper">
             <div className='iso_list'>
-                <button>
+                <button onClick={openModal}>
                     <img src={PlusImage}></img>
                 </button>
                 <div className='iso_card'>
@@ -37,6 +53,18 @@ export function IsoScreen(){
                 <p>Descrição de iso selecionada</p>
             </div>
 
+            {isModalOpen && (
+                <div className="modalBackdrop">
+                    <div className="modalCenter">
+                        <div className="modal">
+                            <div className="modal_icons">
+                                <FiX onClick={closeModal} size={30}></FiX>
+                            </div>
+                            <IsoForm close_modal_function = {closeModal}></IsoForm>
+                        </div>
+                    </div>
+                </div>
+            )}
             
         </div>
 
---
+++ src/isoFormulary/isoForm.tsx
@@ -0,0 +1,10 @@
+
+import './isoFormStyle.scss'
+
+export function IsoForm({close_modal_function}:any){
+    return(
+        <div className='iso_form_wrapper'>
+            <h1>OLA</h1>
+        </div>
+    )
+}
