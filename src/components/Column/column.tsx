b3876cb7b1418cd571452328822542c6f846e3f8
pedro11pucci <pedro11pucci@gmail.com>
2023-11-09 21:14:41 -0300

    feat: show create task button only with permission

--- src/components/Column/column.tsx
+++ src/components/Column/column.tsx
@@ -5,40 +5,44 @@ import './column.scss';
 import { BiDotsHorizontalRounded } from 'react-icons/bi';
 import { Add_card } from '../Add_card/add_card';
 import TaskInterface from '../../Interfaces/Interfaces';
+import { useUser } from '../../contexts/UserContext';
 
 interface ColumnProps {
-  col: {
-    name: string;
-    id: string;
-    list: TaskInterface[];
-  };
-  openModal: () => void;
-  openModalCard: (task: TaskInterface|null) => void;
+    col: {
+        name: string;
+        id: string;
+        list: TaskInterface[];
+    };
+    openModal: () => void;
+    openModalCard: (task: TaskInterface | null) => void;
 }
 
 const Column: React.FC<ColumnProps> = (props) => {
-  return (
-    <div className='column_container'>
-      <section className='title-container'>
-        <h4 className='column_title'>{props.col.name}</h4>
-        <div className='dots-menu'>
-          <BiDotsHorizontalRounded size={35} />
+
+    const { user } = useUser()
+
+    return (
+        <div className='column_container'>
+            <section className='title-container'>
+                <h4 className='column_title'>{props.col.name}</h4>
+                <div className='dots-menu'>
+                    <BiDotsHorizontalRounded size={35} />
+                </div>
+            </section>
+            <hr></hr>
+            <Droppable droppableId={props.col.id}>
+                {(provided) => (
+                    <div className='column_list' ref={provided.innerRef}>
+                        {props.col.list.map((task, index) => (
+                            <Card key={task.id} text={task.name} openModalCard={props.openModalCard} index={index} task={task} />
+                        ))}
+                        {provided.placeholder}
+                        {user.role.create_task && props.col.id === 'todo' && <Add_card openModal={props.openModal} />}
+                    </div>
+                )}
+            </Droppable>
         </div>
-      </section>
-      <hr></hr>
-      <Droppable droppableId={props.col.id}>
-        {(provided) => (
-          <div className='column_list' ref={provided.innerRef}>
-            {props.col.list.map((task, index) => (
-              <Card key={task.id} text={task.name} openModalCard={props.openModalCard} index={index} task={task} />
-            ))}
-            {provided.placeholder}
-            {props.col.id === 'todo' && <Add_card openModal={props.openModal} />}
-          </div>
-        )}
-      </Droppable>
-    </div>
-  );
+    );
 };
 
 export default Column;
